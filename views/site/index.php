c2575b7237fa9a815d4ccaacdad0da468b8aaecf
DmitryAlex2012 <tpmfd27@gmail.com>
2020-05-05 17:46:28 +0300

    Initial commit

---
+++ views/site/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+
+        <h1>Pattern "Factory Method"!</h1>
+
+        <br>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Select Facebook</a></p>
+
+        <br>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Select LinkedIn</a></p>
+
+    </div>
+
+</div>
